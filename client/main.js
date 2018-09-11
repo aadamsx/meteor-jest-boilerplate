07903dd5adfabcec0ce731a86b8d38b14a349438
Olivier JM <manolivier93@gmail.com>
2018-09-11 13:09:01 +0200

    added login component

--- client/main.js
+++ client/main.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Meteor } from 'meteor/meteor'
 import App from '../imports/ui/App'
-import './main.html';
+import './main.html'
+import 'antd/dist/antd.css'
+
 
 Meteor.startup(() => {
   render(<App/>, document.getElementById('app'))
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Login from './Accounts/Login'
 
 const App = () => (
-    <div>
-        Hello There I am from React
-    </div>
+    <>
+        <Login/>
+    </>
 )
 
 export default App
