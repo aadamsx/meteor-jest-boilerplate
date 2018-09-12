acb41d5c71707dec998362b614a867289996f8a6
Olivier JM <manolivier93@gmail.com>
2018-09-12 11:47:21 +0200

    removed unecessary packages

--- client/main.tsx
+++ client/main.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import { render } from 'react-dom'
 import { Meteor } from 'meteor/meteor'
 import App from '../imports/ui/App'
-import 'antd/dist/antd.css'
 
 
 Meteor.startup(() => {
