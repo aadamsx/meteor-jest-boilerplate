e39178a42d1c9f5c51cc5281ab6374f877221089
Olivier JM <manolivier93@gmail.com>
2018-09-12 09:55:32 +0200

    replaced meteor's bundler with webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const meteorExternals = require('webpack-meteor-externals');
+
+const clientConfig = {
+  entry: './client/main.tsx',
+  module: {
+    rules: [{
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/main.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts']
+  },
+  externals: [
+    meteorExternals()
+  ],
+  devServer: {
+    hot: true
+  }
+};
+
+const serverConfig = {
+  entry: [
+    './server/main.ts'
+  ],
+  target: 'node',
+  devServer: {
+    hot: true
+  },
+  externals: [
+    meteorExternals()
+  ]
+};
+
+module.exports = [clientConfig, serverConfig];
