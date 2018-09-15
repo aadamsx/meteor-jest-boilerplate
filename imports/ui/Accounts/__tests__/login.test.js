0aec6d3054592e6a0fb9e6367e0f061043f91639
Olivier JM <manolivier93@gmail.com>
2018-09-15 23:59:27 +0200

    removed typescripts and added testSetup with mocks

--- imports/ui/Accounts/__tests__/login.test.js
+++ imports/ui/Accounts/__tests__/login.test.js
@@ -5,5 +5,4 @@ import Login from '../Login';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Login />, div)
-//   ReactDOM.unmountComponentAtNode(div);
 });
