b3c0d78a86abf7d5b2508c3709f079ae0f0ab08b
Olivier JM <manolivier93@gmail.com>
2018-09-17 12:15:18 +0200

    implemented eslint with prettier for cleaner code

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Meteor } from 'meteor/meteor'
-import App from '../imports/ui/App'
-
+import React from 'react';
+import { render } from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import App from '../imports/ui/App';
 
 Meteor.startup(() => {
-  render(<App/>, document.getElementById('app'))
-})
+  render(<App />, document.getElementById('app'));
+});
--- imports/ui/Accounts/Login.jsx
+++ imports/ui/Accounts/Login.jsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
 
-class LoginPage extends React.Component{
-render() {
+class LoginPage extends React.Component {
+  render() {
     return (
         <React.Fragment>
             <h2>
                 Login Page
             </h2>
         </React.Fragment>
-        )
+    )
   }
 }
 export default LoginPage
